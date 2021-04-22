9463bd27c860dca41a370ab8bb95672b7d45f8b6
juliansarmien18 <julian010422@gmail.com>
2021-04-22 12:36:12 -0500

    actualizacion con ejemplo del profesor

--- boot.asm
+++ boot.asm
@@ -1,7 +1,26 @@
-inicio:
-    jmp inicio
+; boot.asm
+        mov     ax, 0x07c0
+        mov     ds, ax
 
-    times       510-($-$$)      db      0
-                                db      0x55
-                                db      0xAA
+        mov     si, msg
+        cld
 
+
+
+ch_loop:
+        lodsb
+
+        or      al, al 
+        jz      hang
+        mov     ah, 0x0E
+        mov     bh, 0
+        int     0x10
+        jmp     ch_loop
+
+        hang:
+        jmp     hang
+
+        msg     db 'Arranque de mi SO', 13, 10, 0
+        times   510-($-$$) db 0
+        db      0x55
+        db      0xAA
